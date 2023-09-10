f5b752ae18b6ef43526eb1535783e05f0e2223cb
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-09-10 23:07:15 +0200

    Hardened version check

--- app/Util/Web/Updater.cs
+++ app/Util/Web/Updater.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Reflection;
 
+using Nefarius.Utilities.Assembly;
+
 using Newtonsoft.Json;
 
 using Serilog;
@@ -18,12 +20,12 @@ public static class Updater
     /// <summary>
     ///     Gets the control application version.
     /// </summary>
-    public static Version AssemblyVersion => Assembly.GetEntryAssembly().GetName().Version;
+    private static Version AssemblyVersion => Assembly.GetEntryAssembly()!.GetFileVersion();
 
     /// <summary>
     ///     Gets the releases API URI.
     /// </summary>
-    public static Uri ReleasesUri => new("https://api.github.com/repos/nefarius/Legacinator/releases");
+    private static Uri ReleasesUri => new("https://api.github.com/repos/nefarius/Legacinator/releases");
 
     /// <summary>
     ///     True if tag on latest GitHub release is newer than own assembly version, false otherwise.
@@ -35,42 +37,40 @@ public static class Updater
             try
             {
                 // Query for releases/tags and store information
-                using (WebClient client = new WebClient())
+                using WebClient client = new();
+                Log.Information("Checking for updates, preparing web request");
+
+                // Required or result is HTTP-403
+                client.Headers["User-Agent"] =
+                    "Mozilla/4.0 (Compatible; Windows NT 5.1; MSIE 6.0) " +
+                    "(compatible; MSIE 6.0; Windows NT 5.1; " +
+                    ".NET CLR 1.1.4322; .NET CLR 2.0.50727)";
+
+                // Get body
+                string response = client.DownloadString(ReleasesUri);
+
+                // Get JSON objects
+                IList<Root> result = JsonConvert.DeserializeObject<IList<Root>>(response);
+
+                // Top release is latest of interest
+                Root latest = result.FirstOrDefault();
+
+                // No release found to compare to, bail out
+                if (latest == null)
                 {
-                    Log.Information("Checking for updates, preparing web request");
-
-                    // Required or result is HTTP-403
-                    client.Headers["User-Agent"] =
-                        "Mozilla/4.0 (Compatible; Windows NT 5.1; MSIE 6.0) " +
-                        "(compatible; MSIE 6.0; Windows NT 5.1; " +
-                        ".NET CLR 1.1.4322; .NET CLR 2.0.50727)";
-
-                    // Get body
-                    string response = client.DownloadString(ReleasesUri);
-
-                    // Get JSON objects
-                    IList<Root> result = JsonConvert.DeserializeObject<IList<Root>>(response);
-
-                    // Top release is latest of interest
-                    Root latest = result.FirstOrDefault();
-
-                    // No release found to compare to, bail out
-                    if (latest == null)
-                    {
-                        return false;
-                    }
-
-                    Log.Debug("Latest tag name: {Tag}", latest.TagName);
-
-                    string tag = new string(latest.TagName.Skip(1).ToArray());
-
-                    // Expected format e.g. "v1.2.3" so strip first character
-                    Version version = Version.Parse(tag);
-
-                    bool isOutdated = version.CompareTo(AssemblyVersion) > 0;
-
-                    return isOutdated;
+                    return false;
                 }
+
+                Log.Debug("Latest tag name: {Tag}", latest.TagName);
+
+                string tag = new(latest.TagName.Skip(1).ToArray());
+
+                // Expected format e.g. "v1.2.3" so strip first character
+                Version version = Version.Parse(tag);
+
+                bool isOutdated = version.CompareTo(AssemblyVersion) > 0;
+
+                return isOutdated;
             }
             catch (Exception ex)
             {
